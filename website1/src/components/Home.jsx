f0bd9735fb173b1c49c471f7092054d0ee8dd904
Nidhi-0110 <patelnidhi1628@gmail.com>
2025-03-07 09:55:38 +0530

    reate app

---
+++ website1/src/components/Home.jsx
@@ -0,0 +1,27 @@
+import Table from "./Table"
+
+function Home() {
+    const user = {
+        name : "Hello",
+        age : 21,
+        sub : "maths"
+      }
+
+      const username = "Hii"
+
+    function getData(arr){
+        console.log(arr)
+    }  
+    
+  return (
+    <div>
+      <h1>Home</h1>
+      <h1>{user.name}</h1>
+      <h1>{user.age}</h1>
+      <hr />
+      <Table xyz={user} username={username} getData={getData}/>
+    </div>
+  )
+}
+
+export default Home
